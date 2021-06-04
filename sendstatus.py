1f7bf94470ab277031358b4863410c54362562c3
tilnow <44685516+tilnow@users.noreply.github.com>
2021-06-04 14:54:49 +0300

    Update sendstatus.py

--- sendstatus.py
+++ sendstatus.py
@@ -10,10 +10,11 @@ CK = os.environ.get("CK")
 CS = os.environ.get("CS")
 ATK = os.environ.get("ATK")
 ATS = os.environ.get("ATS")
+myname=os.environ.get("YAK_ROVER_NAME","other-bot")
 
 import twitter
 api = twitter.Api(consumer_key=CK,
                   consumer_secret=CS,
                   access_token_key=ATK,
                   access_token_secret=ATS)
-status = api.PostUpdate('I am alive at '+ str(datetime.now(timezone.utc)))
+status = api.PostUpdate('#{}: '.format(myname)+'Daily status update: I am online.')
